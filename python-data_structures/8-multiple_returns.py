22de6f49613c8a8dd93708871448f236c46c76a0
victor10days <victor10days@gmail.com>
2025-05-17 17:58:22 -0400

    More returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Returns the length of a string and its first character."""
+    if sentence == "":
+        return 0, None
+    else:
+        return len(sentence), sentence[0]
