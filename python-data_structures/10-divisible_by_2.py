d1bd725ff7a9ce78de846dfc7f5f233bb7d861eb
victor10days <victor10days@gmail.com>
2025-05-17 18:41:49 -0400

    Only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    """Print all integers of a list."""
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+def divisible_by_2(my_list=[]):
+    return [n % 2 == 0 for n in my_list]
