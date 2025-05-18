7021f5352a291457b70e6689b324f349bfbc395e
victor10days <victor10days@gmail.com>
2025-05-18 16:41:59 -0400

    I think I fixed it

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except TypeError:
+        except (IndexError, TypeError):
             pass
         except ValueError:
             pass
