dc7cc19216b0d891a932c0c32449d8bf8593755e
victor10days <victor10days@gmail.com>
2025-05-18 16:37:16 -0400

    Corrected

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except IndexError:
         result = None
     finally:
         print("Inside result: {}".format(result))
