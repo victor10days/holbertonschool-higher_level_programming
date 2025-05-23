36dfddd16b26e8ae865f94fc33bb8fff406d465b
victor10days <victor10days@gmail.com>
2025-05-23 12:34:54 -0400

    My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+class Square:
+    """A class that defines a square."""
+    def __init__(self, size=0):
+        """Initialize the square with a size.
+
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
