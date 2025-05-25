724ea6d1d30d4c5c675c1f1748aa4f010a8186f3
victor10days <victor10days@gmail.com>
2025-05-25 14:14:06 -0400

    Area of a square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,6 +19,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """Calculate the area of the square.
 
@@ -26,4 +27,3 @@ class Square:
             int: The area of the square.
         """
         return self.__size ** 2
-    
