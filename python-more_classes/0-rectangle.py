37a02e774c98def48432b16cb894fcd40e4dcb4e
victor10days <victor10days@gmail.com>
2025-05-25 18:33:21 -0400

    Simple rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 class Rectangle:
-    """ Empty class """
-    pass
+    """ Class that defines a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Initialize the rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle. Defaults to 0.
+            height (int): The height of the rectangle. Defaults to 0.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than 0.
+        """
+        self.width = width
+        self.height = height
