59baeda157a2468765d2113f067340761ec23c33
victor10days <victor10days@gmail.com>
2025-05-29 16:30:57 -0400

    My list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+class MyList(list):
+    """A subclass of list that includes a method to print the list in sorted order."""
+    
+    def print_sorted(self):
+        """Prints the list in sorted order."""
+        print(sorted(self))
