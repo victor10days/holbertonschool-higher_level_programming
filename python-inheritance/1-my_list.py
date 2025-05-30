667379f59766ceb87dcefb1ad13066fcf307d70d
victor10days <victor10days@gmail.com>
2025-05-30 11:50:22 -0400

    My List

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines a subclass of list with additional functionality."""
+
 class MyList(list):
     """A subclass of list that includes a method to print the list in sorted order."""
     
