5a1e2eb51d374964a0b4182b6dae64e4ba2e9cd9
victor10days <victor10days@gmail.com>
2025-06-03 18:30:45 -0400

    Student to JSON

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -21,3 +21,16 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): A list of attributes to retrieve. If None,
+                all attributes are retrieved.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
