66611b67841b70895d1b5f04f8788bc39284e398
victor10days <victor10days@gmail.com>
2025-06-03 16:14:19 -0400

    Read File

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """Reads a text file and prints its content to stdout.
-
-    Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
-    """
+    """Reads a text file and prints its content to stdout."""
+    
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content, end='')  # Use end='' to avoid adding an extra newline
