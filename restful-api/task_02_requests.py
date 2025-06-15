8b9d4c867b04febbf0704ef60a779a49343d980e
victor10days <victor10days@gmail.com>
2025-06-15 19:11:43 -0400

    task 02-03

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
