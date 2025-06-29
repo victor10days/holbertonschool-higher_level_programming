f29bbea501bf679e006247113efaab07a1e89fab
victor10days <victor10days@gmail.com>
2025-06-29 17:22:42 -0400

    4. First Table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    age INT NOT NULL
+    id INT,
+    name VARCHAR(255)
 );
 -- This command creates a table named first_table with three columns:
+-- id: an integer that auto-increments and serves as the primary key.
