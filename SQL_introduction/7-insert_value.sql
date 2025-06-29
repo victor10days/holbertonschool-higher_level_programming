8dc7b08fea60c26d5a45d79d261f6538436419ee
victor10days <victor10days@gmail.com>
2025-06-29 17:56:46 -0400

    7. First add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
-INSERT INTO first_table (id, name) VALUES (1, 'John Doe');
--- -- This command inserts a new row into the first_table with id 1 and name 'John Doe'.
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
+-- -- This command inserts a new row into the first_table with id 89 and name 'Best School'.
