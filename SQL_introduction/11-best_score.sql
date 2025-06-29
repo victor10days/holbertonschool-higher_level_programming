aa8e8dabbc54484b60a576f8e5fcfd0fe0e1d0d8
victor10days <victor10days@gmail.com>
2025-06-29 18:43:31 -0400

    11-best_score.sql

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
-SELECT score, name FROM second_table
-ORDER BY score DESC;
--- -- This command retrieves the score and name from second_table, ordering the results by score in descending order.
+-- List records with score >= 12 from second_table, ordered by score descending
+SELECT score, name FROM second_table WHERE score >= 12 ORDER BY score DESC;
