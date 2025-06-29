1a719db6f25f7d8968ec84d7321e180510542bb7
victor10days <victor10days@gmail.com>
2025-06-29 19:05:35 -0400

    15. Number by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,2 @@
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+-- -- -- This command retrieves the score and the count of occurrences of each score from second_table
