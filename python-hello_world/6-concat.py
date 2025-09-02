ff639ffcef243a3b8261ae80acaa5dc5a3638cd7
victor10days <victor10days@gmail.com>
2025-09-02 17:51:33 -0400

    4. Play with strings

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Welcome to"
-str2 = "Holberton School!"
-
-print(str1 + " " + str2)
+str1 = "Holberton"
+str2 = "School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
+print(f"Welcome to {str1} {str2}!")
