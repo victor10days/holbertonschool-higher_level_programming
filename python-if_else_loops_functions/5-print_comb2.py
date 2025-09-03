cc934c577498726baa5ff213faf15a21f8060a80
victor10days <victor10days@gmail.com>
2025-09-03 18:21:57 -0400

    5. corrections

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for i in range(0, 99):
-    print("{}, ".format(i), end="")
-    if i == 98:
-        print("{}".format('99'), end="")
-print()
+for i in range(0, 100):
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i))
