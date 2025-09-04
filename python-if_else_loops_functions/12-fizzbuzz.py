3d908656c7e146f5529d92a887e5fb4224a3f1f7
victor10days <victor10days@gmail.com>
2025-09-03 20:34:54 -0400

    12. corrections

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -6,13 +6,12 @@ def fizzbuzz():
     multiples of five, print Buzz. For numbers which are multiples of both
     three and five, print FizzBuzz.
     """
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
-    print()
+for i in range(1, 101):
+    if i % 15 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        print("Buzz", end=" ")
+    else:
+        print("{}".format(i), end=" ")
