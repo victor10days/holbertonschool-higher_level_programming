c2ec956e9aced42110db98f83a78d243cc041d5c
victor10days <victor10days@gmail.com>
2025-09-03 20:22:00 -0400

    10. a + b

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """Function that adds two integers and returns the result."""
+    new_a = int(a)
+    new_b = int(b)
+    a = new_a
+    b = new_b
+    return a + b
