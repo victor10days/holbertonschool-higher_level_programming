bd2a18f7a63922d2d5fb22cf4f6aefe91a383ea3
victor10days <victor10days@gmail.com>
2025-09-08 14:43:34 -0400

    6. Corrections (outputs)

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,9 +2,22 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    a = input
-    b = input
-    operator = input
+    #Argument count check
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Available operators: +, -, * and /")
+        sys.exit(1)
+    
+    # Argument parsing
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+if operator not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    # Operation execution
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
