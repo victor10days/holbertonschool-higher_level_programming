4bedcce59e10558e6b636a1e69a835b53203d13b
victor10days <victor10days@gmail.com>
2025-09-14 20:29:26 -0400

    4. Corrections

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             division = my_list_1[i] / my_list_2[i]
-        except (TypeError, ValueError):
+        except TypeError:
             division = 0
             print("wrong type")
         except ZeroDivisionError:
