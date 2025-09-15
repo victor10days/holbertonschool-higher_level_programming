d9c27922fb349e79e3a4f29bb29be7bbe4657cb9
victor10days <victor10days@gmail.com>
2025-09-14 20:35:02 -0400

    6. Raise a message

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """Raises a NameError exception with a message.
+
+    Args:
+        message (str): The message to be associated with the NameError.
+    """
+    raise NameError(message)
