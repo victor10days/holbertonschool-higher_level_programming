c046df6afe574d5f10d8f355085e42edc509a020
victor10days <victor10days@gmail.com>
2025-09-14 20:10:30 -0400

    0. Corrections

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list(value):
+def safe_print_integer(value):
     """Prints an integer with "{:d}".format().
 
     Args:
-        value (int): The integer to print.
+        value (any): The value to be printed.
 
     Returns:
-        True if value has been correctly printed (it means the value is an integer)
-        Otherwise, False
+        bool: True if value is an integer, False otherwise.
     """
     try:
         print("{:d}".format(value))
