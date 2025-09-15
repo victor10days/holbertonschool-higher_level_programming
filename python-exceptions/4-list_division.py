8cead9153aec28209a48f8057557615a76d3fcae
victor10days <victor10days@gmail.com>
2025-09-14 20:27:26 -0400

    4. Divide a list

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    """Divides two lists element by element.
+
+    Args:
+        my_list_1 (list): The first list of numbers.
+        my_list_2 (list): The second list of numbers.
+        list_length (int): The number of elements to consider from both lists.
+
+    Returns:
+        list: A new list containing the results of the division. If division
+              cannot be performed for an element, the result is None for that element.
+    """
+    result = []
+    for i in range(list_length):
+        try:
+            division = my_list_1[i] / my_list_2[i]
+        except (TypeError, ValueError):
+            division = 0
+            print("wrong type")
+        except ZeroDivisionError:
+            division = 0
+            print("division by 0")
+        except IndexError:
+            division = 0
+            print("out of range")
+        finally:
+            result.append(division)
+    return result
