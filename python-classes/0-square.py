80d53e675c2c3c7b0f33cc101ba7038b241d509f
victor10days <victor10days@gmail.com>
2025-09-16 17:02:51 -0400

    0. documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""A class that defines a square.  This is an empty class."""
 class Square:
-    """A class that defines a square."""
+
+    """A class that defines a square.  This is an empty class."""
+    
     def __init__(self):
     pass
