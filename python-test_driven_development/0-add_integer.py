2f81feb09ff5fc7250051a049ced58567ead4534
victor10days <victor10days@gmail.com>
2025-09-16 16:19:22 -0400

    0. My first square

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Adds two integers.
+
+    Args:
+        a (int or float): The first integer.
+        b (int or float, optional): The second integer. Defaults to 98.
+
+    Raises:
+        TypeError: If either a or b is not an integer or float.
+
+    Returns:
+        int: The sum of a and b as an integer.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a + b)
