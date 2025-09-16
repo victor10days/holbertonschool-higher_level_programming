bd24cf20810704d530977701d24775a1a61bd147
victor10days <victor10days@gmail.com>
2025-09-16 17:06:31 -0400

    0. corrections

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that defines an empty Square class."""
 class Square:
-    """A class that defines a square.  This is an empty class."""
+    """A class that defines a square."""
     pass
