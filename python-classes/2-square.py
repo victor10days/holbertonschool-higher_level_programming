233c8c268eb90148c95a291c92dd207279700602
victor10days <victor10days@gmail.com>
2025-09-16 17:29:58 -0400

    2. Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""This module defines a Square class with a size attribute."""
+
+
+class Square:
+    """A class that defines a square."""
+    pass
+
+    def __init__(self, size):
+        """Initialize the square with a given size.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
