cb0bdb04c2b89896fe3ac0e9cbf62639095dfdd0
victor10days <victor10days@gmail.com>
2025-09-20 18:11:01 -0400

    7. Pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -59,10 +59,10 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Return a string representation of the rectangle"""
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return "\n".join([str(self.print_symbol) * self.__width 
+    """Return a string representation of the rectangle"""
+    if self.__width == 0 or self.__height == 0:
+        return ""
+      return "\n".join([str(self.print_symbol) * self.__width
                         for _ in range(self.__height)])
 
     def __repr__(self):
