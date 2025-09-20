07d81f11f9a499a4d5b3592ea937306e432c7197
victor10days <victor10days@gmail.com>
2025-09-20 18:08:05 -0400

    7. Pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     number_of_instances = 0
     print_symbol = "#"
-    
+
     def __init__(self, width=0, height=0):
         """Initialize the rectangle with width and height.
 
