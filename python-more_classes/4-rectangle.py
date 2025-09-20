566e02734ba694dc2bdad9d97b345c30de8d334e
victor10days <victor10days@gmail.com>
2025-09-20 17:39:34 -0400

    4. Corrections

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -60,6 +60,5 @@ class Rectangle:
         return "\n".join(["#" * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
-        """Return a string representation of the rectangle to be able to recreate a new instance by using eval
-        """
+        """Returns string representation of the rectangle to recreate a new instance with eval"""
         return "Rectangle({}, {})".format(self.__width, self.__height)
