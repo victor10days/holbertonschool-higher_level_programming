2a24e23ba752ba2ace577a7730403d27f4bba536
victor10days <victor10days@gmail.com>
2025-09-20 18:01:00 -0400

    7. Corrections

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -18,6 +18,7 @@ class Rectangle:
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
+        self.print_symbol = Rectangle.print_symbol
 
     @property
     def width(self):
@@ -61,7 +62,7 @@ class Rectangle:
         """Return a string representation of the rectangle"""
         if self.__width == 0 or self.__height == 0:
             return ""
-        return "\n".join([print_symbol * self.__width for _ in range(self.__height)])
+        return "\n".join([Rectangle.print_symbol * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """Return eval() compatible representation"""
