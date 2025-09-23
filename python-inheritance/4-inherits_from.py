7a6895c935c55a2a3cd91fb7a8ac91a06acf9deb
victor10days <victor10days@gmail.com>
2025-09-23 14:43:18 -0400

    4. Corrections

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
+"""Module for subclass checking, excluding exact type matches."""
+
+
 def inherits_from(obj, a_class):
-	return isinstance(obj, a_class) and type(obj) != a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
