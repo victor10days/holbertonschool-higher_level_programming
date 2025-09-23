1cecdddcab2a2f016331751293e554972e971f1c
victor10days <victor10days@gmail.com>
2025-09-23 13:59:16 -0400

    1. Corrections

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
-"""Class MyList that inherits from list"""
-
 class MyList(list):
+	"""Class MyList that inherits from list"""
+
+	
 	def print_sorted(self): 
 		print(sorted(self))
