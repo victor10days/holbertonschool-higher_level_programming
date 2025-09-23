98d13b0822f316879a00ba7bc51396b5e93fae5a
victor10days <victor10days@gmail.com>
2025-09-23 14:29:11 -0400

    3. Corrections

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+"""Module for checking class inheritance relationships."""
+
+
 def is_kind_of_class(obj, a_class):
     """Returns True if the object is an instance of, or if the object is
     an instance of a class that inherited from, the specified class;
