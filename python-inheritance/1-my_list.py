eb296da6da20cffe640f8d9e85741601b61d7785
victor10days <victor10days@gmail.com>
2025-09-23 14:05:26 -0400

    1. pep8

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,10 +2,10 @@
 
 """Module that defines MyList class that inherits from list."""
 
+
 class MyList(list):
-	"""Class MyList that inherits from list"""
+    """Class MyList that inherits from list"""
 
-
-	def print_sorted(self): 
-		"""Prints the list in ascending order"""
-		print(sorted(self))
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        print(sorted(self))
