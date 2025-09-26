468b5ead8749c0b8dcf748fdb229ba97d6214c72
victor10days <victor10days@gmail.com>
2025-09-26 15:02:16 -0400

    8. autopep

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """Class rectangle that inherits from BaseGeometry."""
+
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
