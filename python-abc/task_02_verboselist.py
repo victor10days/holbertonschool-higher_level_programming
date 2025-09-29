9a51752ae0a36e8fa0de32204a3404c814198355
victor10days <victor10days@gmail.com>
2025-09-29 16:16:07 -0400

    2. Corrections

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 
-from abc import ABC, abstractmethod
-import list
-
-class VerboseList(list, ABC):
-    @abstractmethod
+class VerboseList(list):
     def append(self, item):
         super().append(item)
-        print(f"Added {item} to the list.")
+        print(f"Added [{item}] to the list.")
 
-    @abstractmethod
     def extend(self, iterable):
-        super().extend(items)
-        print(f"Extended the list with {len(items)} items.")
+        count = len(iterable)
+        super().extend(iterable)
+        print(f"Extended the list with [{count}] items.")
 
-    @abstractmethod
     def remove(self, item):
+        print(f"Removed [{item}] from the list.")
         super().remove(item)
-        print(f"Removed {item} from the list.")
 
-    @abstractmethod
     def pop(self, index=-1):
-        super().pop(index)
-        print(f"Popped item at index {index} from the list.")
+        if index == -1:
+            item = self[-1]
+        else:
+            item = self[index]
+        result = super().pop(index)
+        print(f"Popped [{item}] from the list.")
+        return result
