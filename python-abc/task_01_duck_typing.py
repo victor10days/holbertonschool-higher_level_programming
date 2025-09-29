95909f496d3808e5d5372b96e8a499e583e2900e
victor10days <victor10days@gmail.com>
2025-09-29 15:17:32 -0400

    1. corrections

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -5,41 +5,35 @@ import math
 
 
 class Shape(ABC):
-	@abstractmethod
-	def area(self):
-		pass
-	
-	@abstractmethod
-	def perimeter(self):
-		pass
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
 
 class Circle(Shape):
-	def __init__(self, radius):
-		self.radius = radius
-	
-	def area(self):
-		return math.pi * self.radius ** 2
-	
-	def perimeter(self):
-		return 2 * math.pi * self.radius
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
-	def __init__(self, width, height):
-		self.width = width
-		self.height = height
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
-	def area(self):
-		return self.width * self.height
+    def area(self):
+        return self.width * self.height
 
-	def perimeter(self):
-		return 2 * (self.width +self.height)
+    def perimeter(self):
+        return 2 * (self.width + self.height)
 
 def shape_info(shape):
-	print(f'Area: {shape.area()}')
-	print(f'Perimeter: {shape.perimeter()}')
-
-radius = float(input('Radius: '))
-circle = Circle(radius)
-width = float(input('Width: '))
-height = float(input('Height: '))
-rectangle = Rectangle(width, height)
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
