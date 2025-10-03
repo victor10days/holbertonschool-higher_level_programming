94f10b5a967948d9c3b4b82d9a2e94fa7b214662
victor10days <victor10days@gmail.com>
2025-10-03 17:49:29 -0400

    0.Corrections

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+'''Reads a text file (UTF8) and prints it to sdtout'''
+
+
 def read_file(filename=''):
-	with open('my_file_0.txt', 'r', encoding='utf-8') as f:
-		print(f.read(), end='')
+    with open('my_file_0.txt', 'r', encoding='utf-8') as f:
+        print(f.read(), end='')
