1220979e77504e5423242ddbeead53aa2cc0b9dc
victor10days <victor10days@gmail.com>
2025-10-04 15:07:33 -0400

    7. Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Adds all arguments to a Python list and saves them to a file in JSON format.'''
+
+
+from sys import argv
+import os.path
+from load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+
+
+def add_item(filename, items):
+    '''Adds all arguments to a Python list and saves them to a file in JSON format.'''
+    if os.path.exists(filename):
+        current_items = load_from_json_file(filename)
+    else:
+        current_items = []
+    current_items.extend(items)
+    save_to_json_file(current_items, filename)  
+    return current_items
