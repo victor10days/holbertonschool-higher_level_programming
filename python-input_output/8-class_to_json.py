52e222ec4dee443abb58b2b62b37a159d3b30b9e
victor10days <victor10days@gmail.com>
2025-10-04 15:39:04 -0400

    8. Corrections

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,6 +3,7 @@
 (list, dictionary, string, integer and boolean) for JSON serialization of
 an object.'''
 
+
 def class_to_json(obj):
     '''Returns the dictionary description with simple data structures
     (list, dictionary, string, integer and boolean) for JSON serialization of
