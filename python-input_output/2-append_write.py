c1492a550f747e0578032d40ab432d13b6a500c7
victor10days <victor10days@gmail.com>
2025-10-04 14:08:14 -0400

    2. corrections

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename='', text=''):
 	'''Appends to UTF8 and returns the number of characters written.'''
 	with open (filename, 'a', encoding='utf-8') as f:
-		return f.append_write(text)
+		return f.write(text)
