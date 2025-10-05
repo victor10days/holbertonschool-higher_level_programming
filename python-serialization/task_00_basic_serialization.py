f16e9a1dd2c47838d6f77e44f08408571748862c
victor10days <victor10days@gmail.com>
2025-10-05 00:15:30 -0400

    task 0 corrections

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
+'''Basic serialization and deserialization of JSON.'''
+
+
+import json
+
 def serialize_and_save_to_file(data, filename):
     """Serializes data to JSON and saves it to a file."""
 
-    
-    import json
 
     with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f)
-    return None
+
 
 def load_from_json_file(filename):
     """Loads data from a JSON file and deserializes it."""
 
 
-    import json
-
     with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
-    return None
+
