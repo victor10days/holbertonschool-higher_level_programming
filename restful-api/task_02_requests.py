ea6f2d22f774a0eb743f27a229a9eba9554f9c45
victor10days <victor10days@gmail.com>
2025-10-20 14:56:38 -0400

    2. Corrections

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -6,24 +6,27 @@ import csv
 def fetch_and_print_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    
-    print (f'Status code: {response.status_code}')
+
+    print(f"Status Code: {response.status_code}")
 
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post['title'])
-    else:
-        print(f"Failed to retrieve posts. Status code: {response.status_code}")
 
 def fetch_and_save_posts():
     url = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         posts = response.json()
-        with open("posts.txt", "w") as file:
-            for post in posts:
-                file.write(f"Post ID: {post['id']}, Title: {post['title']}\n")
-    else:
-        print(f"Failed to retrieve posts. Status code: {response.status_code}")
+        data = [
+            {'id': post['id'], 'title': post['title'], 'body': post['body']}
+            for post in posts
+        ]
+        with open('posts.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerows(data)
