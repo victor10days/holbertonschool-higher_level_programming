730b016b8829a7bc8dbca770dd59e660e4f054b5
victor10days <victor10days@gmail.com>
2025-10-20 11:14:58 -0400

    2. Corrections

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,3 +12,15 @@ def fetch_and_print_posts():
             print(f"Post ID: {post['id']}, Title: {post['title']}")
     else:
         print(f"Failed to retrieve posts. Status code: {response.status_code}")
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        with open("posts.txt", "w") as file:
+            for post in posts:
+                file.write(f"Post ID: {post['id']}, Title: {post['title']}\n")
+    else:
+        print(f"Failed to retrieve posts. Status code: {response.status_code}")
