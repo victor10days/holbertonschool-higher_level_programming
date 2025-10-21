b4d890a11808e575430ee2116e8b9c2a99a916e4
victor10days <victor10days@gmail.com>
2025-10-21 19:39:10 -0400

    6. States table

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Script that creates the database hbtn_0d_usa and the table sates --
+--
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
