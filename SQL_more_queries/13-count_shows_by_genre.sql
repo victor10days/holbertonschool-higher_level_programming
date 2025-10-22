80d650089cbe288d0f618d65964e029040808987
victor10days <victor10days@gmail.com>
2025-10-22 19:19:37 -0400

    13. CORRECTIONS

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- counting and grouping data
 --
-SELECT tv_gneres.name AS genre
+SELECT tv_gneres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
