c1835c75f0613655bc533cd74b7d1660576aea8f
victor10days <victor10days@gmail.com>
2025-10-22 19:17:03 -0400

    13. Number of shows by genre

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- counting and grouping data
+--
+SELECT tv_gneres.name AS genre
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
