063322ee932c798419151a2d820a7a20fe34a37a
victor10days <victor10days@gmail.com>
2025-10-22 18:34:13 -0400

    10. Genre ID by show

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Lists all shows with at least one genre linked. --
+--
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
