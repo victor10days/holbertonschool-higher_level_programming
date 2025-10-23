84869f41faa7f65a578dd21c50b30f712b015540
victor10days <victor10days@gmail.com>
2025-10-23 18:27:07 -0400

    1. Corrections

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' Filters states with a name starting with N (upper N) from the database hbtn_0e_0_usa '''
+""" Lists all states with a name starting with N """
 
 import sys
 import MySQLdb
