78e698b0a011a2de4ab40a200dc96166a7b6cd64
victor10days <victor10days@gmail.com>
2025-10-23 17:55:15 -0400

    0.corrections

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 """ Lists all states from the database hbtn_0e_0_usa """
 
-
-import sys
-import MySQLdb
-
 if __name__ == "__main__":
+    import sys
+    import MySQLdb
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
