4cdb41fa3701c2bc6044cd4357fb157a69223715
victor10days <victor10days@gmail.com>
2025-10-23 17:43:59 -0400

    0. corrections

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Lists all states from the database hbtn_0e_0_usa """
 
-
 import sys
 import MySQLdb
 
@@ -14,9 +13,10 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
     states = cursor.fetchall()
     for state in states:
-        print(state)
+    print(state)
     cursor.close()
     db.close()
+    
