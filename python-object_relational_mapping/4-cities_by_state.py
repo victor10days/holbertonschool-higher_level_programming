730ad6db479cb3c169e980bbfafc28e86299d22e
victor10days <victor10days@gmail.com>
2025-10-24 15:15:14 -0400

    4. Cities by states

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Lists all cities from the database hbtn_0e_4_usa """
+
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(""" 
+                SELECT cities.id, cities.name, states.name 
+                FROM cities JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC;
+                """)
+    for row in cur.fetchall():
+        print(row)
+    
+    cur.close()
+    db.close()
