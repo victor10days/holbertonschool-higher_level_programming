661e666b8662a5cbc521ffb94123a650f0a7c0dc
victor10days <victor10days@gmail.com>
2025-10-24 18:16:58 -0400

    4. CORRECTIONS

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Lists all cities from the database hbtn_0e_4_usa """
 
-
 import sys
 import MySQLdb
 
@@ -14,13 +13,9 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute(""" 
-                SELECT cities.id, cities.name, states.name 
-                FROM cities JOIN states ON cities.state_id = states.id
-                ORDER BY cities.id ASC
-                """)
-    for row in cur.fetchall():
-        print(row)
-    
+    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
     cursor.close()
     db.close()
