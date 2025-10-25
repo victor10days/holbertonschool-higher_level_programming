b25ee3b7918943a055a1e617ef858a6a434a77f6
victor10days <victor10days@gmail.com>
2025-10-25 13:18:48 -0400

    6. CORRECTIONS

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -6,6 +6,8 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+
 class State(Base):
     '''State class for SQLAlchemy ORM'''
     __tablename__ = 'states'
