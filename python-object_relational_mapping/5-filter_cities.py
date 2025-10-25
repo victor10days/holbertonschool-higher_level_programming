ed83aaa32b0866cea778519faff29a5c2c84fb1d
victor10days <victor10days@gmail.com>
2025-10-25 11:04:00 -0400

    5. Corrections

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-'''Takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa'''
+'''Takes in the name of a state as an argument and lists all cities of that state.'''
 
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                    port=3306,
+                    user=sys.argv[1],
+                    passwd=sys.argv[2],
+                    db=sys.argv[3])
     cursor = db.cursor()
     query = """SELECT cities.name FROM cities
             JOIN states ON cities.state_id = states.id
