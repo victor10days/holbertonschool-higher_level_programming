92afe3f2ef7950c013aa1bd71a316d25cb10915f
victor10days <victor10days@gmail.com>
2025-10-25 13:15:47 -0400

    6. CORRECTIONS

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
-'''Module for model state'''
+"""Module for model state"""
 
-from sqlalchemy import Coumn, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 class State(Base):
     '''State class for SQLAlchemy ORM'''
     __tablename__ = 'states'
-    id = Coumn(Integer, primary_key=True, nullable=False)
-    name = Coumn(String(128), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
