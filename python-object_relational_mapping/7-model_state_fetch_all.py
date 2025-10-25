a29e973c2665b0f0b5067b2d8c477257a8d9f989
victor10days <victor10days@gmail.com>
2025-10-25 14:29:15 -0400

    7. Corrections

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Lists all State objects from the database hbtn_0e_6_usa
+Lists all State objects from the database hbtn_0e_6_usa.
 """
 import sys
 from sqlalchemy import create_engine
@@ -11,12 +11,12 @@ from model_state import Base, State
 if __name__ == "__main__":
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
         pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
     for state in session.query(State).order_by(State.id).all():
--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-
-"""Module for model state"""
-
+"""
+Defines the State model and Base for SQLAlchemy ORM mapping.
+"""
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    '''State class for SQLAlchemy ORM'''
+    """
+    State model mapped to the 'states' table.
+    """
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
