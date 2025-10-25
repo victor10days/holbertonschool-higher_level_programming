df505bd002f1d885d7fdff9222ba6862de4e74b1
victor10days <victor10days@gmail.com>
2025-10-25 10:25:25 -0400

    5. Corrections

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-'''takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa'''
+'''Takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa'''
 
-
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
