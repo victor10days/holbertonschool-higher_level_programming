ddfb3541a2826874941f3187898dc601406b04d0
victor10days <victor10days@gmail.com>
2025-10-25 14:00:43 -0400

    7. Corrections

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,10 +2,11 @@
 """Script to fetch all State objects from the database"""
 
 
-import sys
-from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import sys
+from model_state import Base, State
+
 
 if __name__ == "__main__":
     engine = create_engine(
