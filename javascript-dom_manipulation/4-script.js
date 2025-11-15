9f813e89bc8afece95bd5b3e017b8d42e46ae397
victor10days <victor10days@gmail.com>
2025-11-14 20:02:14 -0400

    4. List of elements

---
+++ javascript-dom_manipulation/4-script.js
@@ -0,0 +1,5 @@
+document.querySelector('#add_item').addEventListener('click', function() {
+    const ul = document.querySelector('ul.my_list');
+    li.textContent = 'Item';
+    document.querySelector('.my_list').appendChild(li);
+});
